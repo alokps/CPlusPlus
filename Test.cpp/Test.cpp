b323e6d61b8d5bf00c3418b31594150689917a83
alokps <alokps.ece@gmail.com>
2015-10-31 23:55:48 -0500

    Initial baseline Commit

---
+++ Test.cpp/Test.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : Alokps
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+
+using namespace::std;
+
+int main(void) {
+	cout<<"Alokps"<<endl;
+	return EXIT_SUCCESS;
+}
